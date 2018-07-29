44281ad79b8bc22344160d9b5c71786565d4f07f
Sandy Grewal <sgrewal@pivotal.io>
2018-07-29 00:27:49 -0400

    srt updates

---
+++ install-pcf/gcp/tasks/create-infrastructure-srt/task.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+set -eu
+
+root=$PWD
+
+# us: ops-manager-us/pcf-gcp-1.9.2.tar.gz -> ops-manager-us/pcf-gcp-1.9.2.tar.gz
+pcf_opsman_bucket_path=$(grep -i 'us:.*.tar.gz' pivnet-opsmgr/*GCP.yml | cut -d' ' -f2)
+
+# ops-manager-us/pcf-gcp-1.9.2.tar.gz -> opsman-pcf-gcp-1-9-2
+pcf_opsman_image_name=$(echo $pcf_opsman_bucket_path | sed 's%.*/\(.*\).tar.gz%opsman-\1%' | sed 's/\./-/g')
+
+
+
+if [[ ${POE_SSL_NAME1} == "" || ${POE_SSL_NAME1} == "null" ]]; then
+  echo "Generating Self Signed Certs for ${SYSTEM_DOMAIN} & ${APPS_DOMAIN} ..."
+  pcf-pipelines/scripts/gen_ssl_certs.sh "${SYSTEM_DOMAIN}" "${APPS_DOMAIN}"
+  pcf_ert_ssl_cert=$(cat ${SYSTEM_DOMAIN}.crt)
+  pcf_ert_ssl_key=$(cat ${SYSTEM_DOMAIN}.key)
+else
+  pcf_ert_ssl_cert="$POE_SSL_CERT1"
+  pcf_ert_ssl_key="$POE_SSL_KEY1"
+fi
+
+export GOOGLE_CREDENTIALS=${GCP_SERVICE_ACCOUNT_KEY}
+export GOOGLE_PROJECT=${GCP_PROJECT_ID}
+export GOOGLE_REGION=${GCP_REGION}
+
+terraform init pcf-pipelines/install-pcf/gcp/terraform-srt
+
+terraform plan \
+  -var "gcp_proj_id=${GCP_PROJECT_ID}" \
+  -var "gcp_region=${GCP_REGION}" \
+  -var "gcp_zone_1=${GCP_ZONE_1}" \
+  -var "gcp_zone_2=${GCP_ZONE_2}" \
+  -var "gcp_zone_3=${GCP_ZONE_3}" \
+  -var "gcp_storage_bucket_location=${GCP_STORAGE_BUCKET_LOCATION}" \
+  -var "prefix=${GCP_RESOURCE_PREFIX}" \
+  -var "pcf_opsman_image_name=${pcf_opsman_image_name}" \
+  -var "pcf_ert_domain=${PCF_ERT_DOMAIN}" \
+  -var "system_domain=${SYSTEM_DOMAIN}" \
+  -var "apps_domain=${APPS_DOMAIN}" \
+  -var "pcf_ert_ssl_cert=${pcf_ert_ssl_cert}" \
+  -var "pcf_ert_ssl_key=${pcf_ert_ssl_key}" \
+  -out terraform.tfplan \
+  -state terraform-state/terraform.tfstate \
+  pcf-pipelines/install-pcf/gcp/terraform
+
+terraform apply \
+  -state-out $root/create-infrastructure-output/terraform.tfstate \
+  -parallelism=5 \
+  terraform.tfplan
+
+cd $root/create-infrastructure-output
+  output_json=$(terraform output -json -state=terraform.tfstate)
+  pub_ip_global_pcf=$(echo $output_json | jq --raw-output '.pub_ip_global_pcf.value')
+  pub_ip_ssh_and_doppler=$(echo $output_json | jq --raw-output '.pub_ip_ssh_and_doppler.value')
+  pub_ip_ssh_tcp_lb=$(echo $output_json | jq --raw-output '.pub_ip_ssh_tcp_lb.value')
+  pub_ip_opsman=$(echo $output_json | jq --raw-output '.pub_ip_opsman.value')
+cd -
+
+echo "Please configure DNS as follows:"
+echo "----------------------------------------------------------------------------------------------"
+echo "*.${SYSTEM_DOMAIN} == ${pub_ip_global_pcf}"
+echo "*.${APPS_DOMAIN} == ${pub_ip_global_pcf}"
+echo "ssh.${SYSTEM_DOMAIN} == ${pub_ip_ssh_and_doppler}"
+echo "doppler.${SYSTEM_DOMAIN} == ${pub_ip_ssh_and_doppler}"
+echo "loggregator.${SYSTEM_DOMAIN} == ${pub_ip_ssh_and_doppler}"
+echo "tcp.${PCF_ERT_DOMAIN} == ${pub_ip_ssh_tcp_lb}"
+echo "opsman.${PCF_ERT_DOMAIN} == ${pub_ip_opsman}"
+echo "----------------------------------------------------------------------------------------------"
--- tasks/configure-srt/task.sh
+++ tasks/configure-srt/task.sh
@@ -152,6 +152,7 @@ cf_resources=$(
       "control": {"internet_connected": $internet_connected},
       "database": {"internet_connected": $internet_connected},
       "router": {"internet_connected": $internet_connected},
+      "blobstore": {"internet_connected": $internet_connected}
     }
 
     |
@@ -193,31 +194,31 @@ cf_properties=$(
     --arg system_domain "$SYSTEM_DOMAIN"\
     --arg apps_domain "$APPS_DOMAIN" \
     --arg mysql_monitor_recipient_email "$mysql_monitor_recipient_email" \
-    --arg db_host "$db_host" \
-    --arg db_locket_username "$db_locket_username" \
-    --arg db_locket_password "$db_locket_password" \
-    --arg db_silk_username "$db_silk_username" \
-    --arg db_silk_password "$db_silk_password" \
-    --arg db_app_usage_service_username "$db_app_usage_service_username" \
-    --arg db_app_usage_service_password "$db_app_usage_service_password" \
-    --arg db_autoscale_username "$db_autoscale_username" \
-    --arg db_autoscale_password "$db_autoscale_password" \
-    --arg db_diego_username "$db_diego_username" \
-    --arg db_diego_password "$db_diego_password" \
-    --arg db_notifications_username "$db_notifications_username" \
-    --arg db_notifications_password "$db_notifications_password" \
-    --arg db_routing_username "$db_routing_username" \
-    --arg db_routing_password "$db_routing_password" \
-    --arg db_uaa_username "$db_uaa_username" \
-    --arg db_uaa_password "$db_uaa_password" \
-    --arg db_ccdb_username "$db_ccdb_username" \
-    --arg db_ccdb_password "$db_ccdb_password" \
-    --arg db_accountdb_username "$db_accountdb_username" \
-    --arg db_accountdb_password "$db_accountdb_password" \
-    --arg db_networkpolicyserverdb_username "$db_networkpolicyserverdb_username" \
-    --arg db_networkpolicyserverdb_password "$db_networkpolicyserverdb_password" \
-    --arg db_nfsvolumedb_username "$db_nfsvolumedb_username" \
-    --arg db_nfsvolumedb_password "$db_nfsvolumedb_password" \
+#    --arg db_host "$db_host" \
+ #   --arg db_locket_username "$db_locket_username" \
+ #   --arg db_locket_password "$db_locket_password" \
+ #   --arg db_silk_username "$db_silk_username" \
+ #   --arg db_silk_password "$db_silk_password" \
+ #   --arg db_app_usage_service_username "$db_app_usage_service_username" \
+ #   --arg db_app_usage_service_password "$db_app_usage_service_password" \
+ #   --arg db_autoscale_username "$db_autoscale_username" \
+ #   --arg db_autoscale_password "$db_autoscale_password" \
+ #   --arg db_diego_username "$db_diego_username" \
+ #   --arg db_diego_password "$db_diego_password" \
+ #   --arg db_notifications_username "$db_notifications_username" \
+ #   --arg db_notifications_password "$db_notifications_password" \
+ #   --arg db_routing_username "$db_routing_username" \
+ #   --arg db_routing_password "$db_routing_password" \
+ #   --arg db_uaa_username "$db_uaa_username" \
+ #   --arg db_uaa_password "$db_uaa_password" \
+ #   --arg db_ccdb_username "$db_ccdb_username" \
+ #   --arg db_ccdb_password "$db_ccdb_password" \
+ #   --arg db_accountdb_username "$db_accountdb_username" \
+ #   --arg db_accountdb_password "$db_accountdb_password" \
+ #   --arg db_networkpolicyserverdb_username "$db_networkpolicyserverdb_username" \
+ #   --arg db_networkpolicyserverdb_password "$db_networkpolicyserverdb_password" \
+ #   --arg db_nfsvolumedb_username "$db_nfsvolumedb_username" \
+ #   --arg db_nfsvolumedb_password "$db_nfsvolumedb_password" \
     --arg s3_endpoint "$S3_ENDPOINT" \
     --arg aws_access_key "${aws_access_key:-''}" \
     --arg aws_secret_key "${aws_secret_key:-''}" \
@@ -241,36 +242,36 @@ cf_properties=$(
       ".properties.container_networking_interface_plugin.silk.network_cidr": { "value": $container_networking_nw_cidr },
       ".properties.route_services.enable.ignore_ssl_cert_verification": { "value": true },
       ".properties.security_acknowledgement": { "value": $security_acknowledgement },
-      ".properties.system_database": { "value": "external" },
-      ".properties.system_database.external.port": { "value": "3306" },
-      ".properties.system_database.external.host": { "value": $db_host },
-      ".properties.system_database.external.app_usage_service_username": { "value": $db_app_usage_service_username },
-      ".properties.system_database.external.app_usage_service_password": { "value": { "secret": $db_app_usage_service_password } },
-      ".properties.system_database.external.autoscale_username": { "value": $db_autoscale_username },
-      ".properties.system_database.external.autoscale_password": { "value": { "secret": $db_autoscale_password } },
-      ".properties.system_database.external.diego_username": { "value": $db_diego_username },
-      ".properties.system_database.external.diego_password": { "value": { "secret": $db_diego_password } },
-      ".properties.system_database.external.notifications_username": { "value": $db_notifications_username },
-      ".properties.system_database.external.notifications_password": { "value": { "secret": $db_notifications_password } },
-      ".properties.system_database.external.routing_username": { "value": $db_routing_username },
-      ".properties.system_database.external.routing_password": { "value": { "secret": $db_routing_password } },
-      ".properties.system_database.external.ccdb_username": { "value": $db_ccdb_username },
-      ".properties.system_database.external.ccdb_password": { "value": { "secret": $db_ccdb_password } },
-      ".properties.system_database.external.account_username": { "value": $db_accountdb_username },
-      ".properties.system_database.external.account_password": { "value": { "secret": $db_accountdb_password } },
-      ".properties.system_database.external.networkpolicyserver_username": { "value": $db_networkpolicyserverdb_username },
-      ".properties.system_database.external.networkpolicyserver_password": { "value": { "secret": $db_networkpolicyserverdb_password } },
-      ".properties.system_database.external.nfsvolume_username": { "value": $db_nfsvolumedb_username },
-      ".properties.system_database.external.nfsvolume_password": { "value": { "secret": $db_nfsvolumedb_password } },
-      ".properties.system_database.external.locket_username": { "value": $db_locket_username },
-      ".properties.system_database.external.locket_password": { "value": { "secret": $db_locket_password } },
-      ".properties.system_database.external.silk_username": { "value": $db_silk_username },
-      ".properties.system_database.external.silk_password": { "value": { "secret": $db_silk_password } },
-      ".properties.uaa_database": { "value": "external" },
-      ".properties.uaa_database.external.host": { "value": $db_host },
-      ".properties.uaa_database.external.port": { "value": "3306" },
-      ".properties.uaa_database.external.uaa_username": { "value": $db_uaa_username },
-      ".properties.uaa_database.external.uaa_password": { "value": { "secret": $db_uaa_password } },
+      ".properties.system_database": { "value": "internal" },
+#      ".properties.system_database.external.port": { "value": "3306" },
+#      ".properties.system_database.external.host": { "value": $db_host },
+#      ".properties.system_database.external.app_usage_service_username": { "value": $db_app_usage_service_username },
+#      ".properties.system_database.external.app_usage_service_password": { "value": { "secret": $db_app_usage_service_password } },
+#      ".properties.system_database.external.autoscale_username": { "value": $db_autoscale_username },
+#      ".properties.system_database.external.autoscale_password": { "value": { "secret": $db_autoscale_password } },
+#      ".properties.system_database.external.diego_username": { "value": $db_diego_username },
+#      ".properties.system_database.external.diego_password": { "value": { "secret": $db_diego_password } },
+#      ".properties.system_database.external.notifications_username": { "value": $db_notifications_username },
+#      ".properties.system_database.external.notifications_password": { "value": { "secret": $db_notifications_password } },
+#      ".properties.system_database.external.routing_username": { "value": $db_routing_username },
+#      ".properties.system_database.external.routing_password": { "value": { "secret": $db_routing_password } },
+#      ".properties.system_database.external.ccdb_username": { "value": $db_ccdb_username },
+#      ".properties.system_database.external.ccdb_password": { "value": { "secret": $db_ccdb_password } },
+#      ".properties.system_database.external.account_username": { "value": $db_accountdb_username },
+#      ".properties.system_database.external.account_password": { "value": { "secret": $db_accountdb_password } },
+#      ".properties.system_database.external.networkpolicyserver_username": { "value": $db_networkpolicyserverdb_username },
+#      ".properties.system_database.external.networkpolicyserver_password": { "value": { "secret": $db_networkpolicyserverdb_password } },
+#      ".properties.system_database.external.nfsvolume_username": { "value": $db_nfsvolumedb_username },
+#      ".properties.system_database.external.nfsvolume_password": { "value": { "secret": $db_nfsvolumedb_password } },
+#      ".properties.system_database.external.locket_username": { "value": $db_locket_username },
+#      ".properties.system_database.external.locket_password": { "value": { "secret": $db_locket_password } },
+#      ".properties.system_database.external.silk_username": { "value": $db_silk_username },
+#      ".properties.system_database.external.silk_password": { "value": { "secret": $db_silk_password } },
+      ".properties.uaa_database": { "value": "internal" },
+#      ".properties.uaa_database.external.host": { "value": $db_host },
+#      ".properties.uaa_database.external.port": { "value": "3306" },
+#      ".properties.uaa_database.external.uaa_username": { "value": $db_uaa_username },
+#      ".properties.uaa_database.external.uaa_password": { "value": { "secret": $db_uaa_password } },
       ".properties.push_apps_manager_company_name": { "value": "pcf-\($iaas)" },
       ".cloud_controller.system_domain": { "value": $system_domain },
       ".cloud_controller.apps_domain": { "value": $apps_domain },
@@ -320,13 +321,13 @@ cf_properties=$(
       }
     elif $iaas == "gcp" then
       {
-        ".properties.system_blobstore": { "value": "external_gcs" },
-        ".properties.system_blobstore.external_gcs.buildpacks_bucket": { "value": "\($terraform_prefix)-buildpacks" },
-        ".properties.system_blobstore.external_gcs.droplets_bucket": { "value": "\($terraform_prefix)-droplets" },
-        ".properties.system_blobstore.external_gcs.packages_bucket": { "value": "\($terraform_prefix)-packages" },
-        ".properties.system_blobstore.external_gcs.resources_bucket": { "value": "\($terraform_prefix)-resources" },
-        ".properties.system_blobstore.external_gcs.access_key": { "value": $gcp_storage_access_key },
-        ".properties.system_blobstore.external_gcs.secret_key": { "value": { "secret": $gcp_storage_secret_key } }
+        ".properties.system_blobstore": { "value": "internal" }  # for srt
+#        ".properties.system_blobstore.external_gcs.buildpacks_bucket": { "value": "\($terraform_prefix)-buildpacks" },
+#        ".properties.system_blobstore.external_gcs.droplets_bucket": { "value": "\($terraform_prefix)-droplets" },
+#        ".properties.system_blobstore.external_gcs.packages_bucket": { "value": "\($terraform_prefix)-packages" },
+#        ".properties.system_blobstore.external_gcs.resources_bucket": { "value": "\($terraform_prefix)-resources" },
+#        ".properties.system_blobstore.external_gcs.access_key": { "value": $gcp_storage_access_key },
+#        ".properties.system_blobstore.external_gcs.secret_key": { "value": { "secret": $gcp_storage_secret_key } }
       }
     else
       .
